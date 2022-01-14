13704d41e2df80c88f7d01429ab9db7669db8ffd
student200036 <200036ww@gmail.com>
2022-01-14 10:06:43 +0900

    commit

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
 const express = require('express')
 const app = express()
 
-const http= require('http').createServer(app)
+const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+
 const dotenv = require('dotenv')
 dotenv.config()
 const host = process.env.HOST
 const port = process.env.PORT
 
-const uuidv4 = require('uuid').v4()
+const uuidv4 = require('uuid').v4
 let users = {}
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+
 io.on('connection', (socket) => {
     socket.on('auth', (user) => {
-        if(user.token) return
+        if (user.token) return
         user.token = uuidv4()
         users[socket.id] = user
         let data = {
             user: user,
-            users: users
+            users: users,
         }
         console.log(data)
         socket.emit('logined', data)
         socket.broadcast.emit('user_joined', data)
     })
-    
-    socket.on('message',(data) => {
+
+    socket.on('message', (data) => {
         console.log(data)
         data.datetime = Date.now()
         io.emit('message', data)
@@ -47,11 +49,11 @@ io.on('connection', (socket) => {
         })
     }
 
-    socket.on('logout', (socket) => {
+    socket.on('logout', () => {
         logout(socket)
     })
 })
 
-http.listen(port,host, () => {
+http.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
 })
